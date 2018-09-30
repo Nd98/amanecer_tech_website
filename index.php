5eeabf961012b609137237391dc39cc4c91855b6
nd98 <nayan.dagliya100922@marwadiuniversity.ac.in>
2018-09-30 22:28:55 +0530

    index.php commited

--- index.php
+++ index.php
@@ -18,7 +18,6 @@
 <link href="css/plugins/magnific-popup.css" rel="stylesheet">
 <link href="css/plugins/bootstrap-datetimepicker.css" rel="stylesheet">
 <link href="css/custom.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
